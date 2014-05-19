26068e54c5e77acc227485ccb2b82034be8770aa
Craig Hess <craig@dazium.com>
2014-05-18 23:19:41 -0500

    Updated behaviors using turbolinks.

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,26 +1,9 @@
 $(function() {
-  // Turbolinks.enableTransitionCache();
-
-  $(document).on('page:before-change', function(e) {
-    console.log('page loaded!');
-    console.log(e);
-    $('body').addClass('fadeout').removeClass('fadein');
-  });
 
   $(document).on('page:update', function(e) {
-    console.log('page loaded!');
-    console.log(e);
-
     $('body').find('.content-area').addClass('will-fadein');
-  });
 
-  $(document).on('page:load', function(e) {
-    console.log('page loaded!');
-    console.log(e);
-
-    $('body').removeClass('fadeout').addClass('fadein');
-
-    var fadeInDelay = 150;
+    var fadeInDelay = 200;
     $('.will-fadein').each(function(i) {
       var $el = $(this);
       setTimeout(function() {
