1eabe10ad15017641b26074b00dd4ee26e5f56e9
Craig Hess <craig@dazium.com>
2014-06-05 15:37:34 -0500

    Added expand / collapse text. Improved posiiton of expanded / collapsed text. Removed old table expander code.

--- contents/js/main.js
+++ contents/js/main.js
@@ -13,30 +13,21 @@ $(function() {
   });
 
   $(document).on('click', '#our-solutions .solutions .solution header', function(e) {
-    var anim_delay = 400;
-    var $solution  = $(this).parents('.solution');
-    var $compacted = $solution.find('.solution-compacted');
-    var $expanded  = $solution.find('.solution-expanded');
+    var anim_delay = 400,
+        $solution  = $(this).parents('.solution'),
+        $compacted = $solution.find('.solution-compacted'),
+        $expanded  = $solution.find('.solution-expanded');
+
     $solution.toggleClass('expanded');
 
     if ($solution.hasClass('expanded')) {
+      $solution.find('header .expand').text('COLLAPSE');
       $expanded.show();
     }
     else {
       setTimeout(function() { $expanded.hide(); }, anim_delay);
+      $solution.find('header .expand').text('EXPAND');
     }
   });
 
-  $(document).on('click', '#solutions-overview .solution-overview table thead tr th:first-child', function(e) {
-    var $headcell = $(this),
-        $table = $headcell.parents('table');
-
-    if ($table.hasClass('expanded')) {
-      $table.find('th,td').not($headcell).fadeToggle(function() { $table.removeClass('expanded'); });
-    }
-    else {
-      $table.addClass('expanded');
-      $table.find('th,td').not($headcell).fadeToggle();
-    }
-  });
 });
